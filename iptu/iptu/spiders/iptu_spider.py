26f9432974070a724a9d7e371bbd2aa69fd24c02
Pedro Markun <pedro@markun.com.br>
2016-01-27 14:06:01 -0200

    fix no resume

--- iptu/iptu/spiders/iptu_spider.py
+++ iptu/iptu/spiders/iptu_spider.py
@@ -12,20 +12,23 @@ class IptuSpider(scrapy.Spider):
     
     
     def start_requests(self):
-        if not self.state.has_key('setor'):
+        if not self.state.has_key('lote'):
             self.state['setor'] = 0
             self.state['quadra'] = 0
             self.state['lote'] = 0
     
         print "Starting in..." + ",".join([str(self.state['setor']).zfill(3),str(self.state['quadra']).zfill(4),str(self.state['lote']).zfill(4)])
+        
         for setor in range(self.state['setor'],311):
-            for quadra in range(self.state['quadra'],1000):
-                for lote in range(self.state['lote'],10000):
+            for quadra in range(self.state['quadra'],100):
+                for lote in range(self.state['lote'],100):
                     payload = { 'pCdSetor': str(setor).zfill(3), 'pCdQuadra': str(quadra).zfill(4), 'pCdLote': str(lote).zfill(4) }
                     self.state['setor'] = setor
                     self.state['quadra'] = quadra
                     self.state['lote'] = lote
                     yield Request(url, self.parse_data, method="POST", body=str(payload), headers={"Content-Type":"application/json"})
+                self.state['lote'] = 0
+            self.state['quadra'] = 0
 
     def parse_data(self, response):
         # do stuff with data...
